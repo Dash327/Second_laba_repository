7e737c78ffff8ea870e7e924dc6c12974afeb9fa
Mityashina Daria <mityashina.06@list.ru>
2025-11-18 15:36:12 +0300

    добавили функцию, ктороая ищет все вхождения времени в тексте; добавили вывод меню

--- time_finder.py
+++ time_finder.py
@@ -6,3 +6,15 @@ import os
 # Регулярное выражение для времени ЧЧ:ММ:СС
 TIME_PATTERN = r"\b(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d\b"
 TIME_REGEX = re.compile(TIME_PATTERN)
+
+
+def find_times_in_text(text: str):
+    return TIME_REGEX.findall(text)
+
+
+def main():
+    print("Поиск времени в формате ЧЧ:ММ:СС")
+    print("Выберите режим:")
+    print("1. Пользовательский ввод")
+    print("2. Поиск на веб-странице (по URL)")
+    print("3. Поиск в локальном файле")
